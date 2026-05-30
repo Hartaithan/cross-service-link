57381a612a68d0c0c9051c28b2569c1939903e37
Hartaithan. <hartaithan@gmail.com>
2026-05-30 18:39:24 +0900

    small widget layout tweaks

--- src/widget/index.ts
+++ src/widget/index.ts
@@ -66,7 +66,7 @@ export class CrossServiceLink {
         (item, i) => `
         <button class="csl-widget-item" data-index="${i}">
           <div class="csl-widget-item-text">
-            <div class="csl-widget-item-title">${item.title}</div>
+            <div class="csl-widget-item-title">${item.short_title}</div>
             <div class="csl-widget-item-desc">${item.description}</div>
           </div>
           <svg class="csl-widget-item-arrow" xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
